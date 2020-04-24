4463a402d21386d676b196b2de4e1635a383afd4
User <User@DESKTOP-8J6C9U7>
2020-04-24 11:08:55 +0530

    1st commit

---
+++ myproject/src/myproject/Customer.java
@@ -0,0 +1,15 @@
+package myproject;
+
+public class Customer {
+	public static void main(String[] args) {
+		int a = 2;
+		int b = 3;
+		int sum=a+b;
+		System.out.println(sum);
+		
+		
+		
+		
+}
+
+}
---
+++ myproject/src/myproject/Mypack.java
@@ -0,0 +1,9 @@
+package myproject;
+
+public class Mypack {
+	public static void main(String[] args) {
+		System.out.println("hello world");
+		
+	}
+
+}
